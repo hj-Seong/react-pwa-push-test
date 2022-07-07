d9554a3e53f40487da87c8bdea8d84eed8080821
hjseong <hjseong1222@gmail.com>
2022-07-07 09:36:29 +0900

    push-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>프로그레시브 웹앱</h1>
+        <p>푸시 테스트를 하고 있습니다</p>
       </header>
     </div>
   );
